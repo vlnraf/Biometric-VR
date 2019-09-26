039712b7b73144a4c55cc3b552feadbc0c6f57d0
vlnraf <raffaele.villani17@gmail.com>
2019-09-26 20:52:48 +0200

    Aggiornamento script concatenazione

--- ConcatenaCSV.py
+++ ConcatenaCSV.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import glob
+import sys
 
-path = 'dati/Emilio' # use your path
+path = 'dati/'+ str(sys.argv[1]) # use your path
 all_files = glob.glob(path + "/*.csv")
 
 li = []
 
 for filename in all_files:
-    if filename == 'dati/Emilio/1_3Emilio.csv':
+    if filename == 'dati/'+str(sys.argv[1])+'/1_3'+str(sys.argv[1])+'.csv':
         continue
     df = pd.read_csv(filename, index_col=None, header=0)
     li.append(df)
@@ -15,9 +16,9 @@ for filename in all_files:
 result = pd.concat(li, axis = 1, sort = False)
 
 
-#combined_csv = pd.concat(li, axis=0, ignore_index=True, sort = False)
+#combined_csv = pd.concat(li, axis=0, ignore_index=True, sort = False) per combinare più files uno sotto l' altro
 
-result.to_csv("dati/datiCompleti/Emiliocombined_csv.csv", index=False, encoding='utf-8-sig')
+result.to_csv('dati/datiCompleti/'+str(sys.argv[1])+'.csv', index=False, encoding='utf-8-sig')
 
 #combined_csv.to_csv("dati/Emilio/combined_csv.csv", index=False, encoding='utf-8-sig')
 
