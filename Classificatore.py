6141a05ca9bba9e254b7e884b056ff3fa1a3a97e
vlnraf <raffaele.villani17@gmail.com>
2019-09-27 17:29:59 +0200

    Risulto Bug Classificatore

--- Classificatore.py
+++ Classificatore.py
@@ -2,6 +2,7 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 data = pandas.read_csv('dati/datiCompleti/dataset.csv', usecols = ['distanza','distanzaDx','distanzaSx'])
 predict = pandas.read_csv('dati/datiCompleti/dataset.csv', usecols = ['Sesso'])
@@ -10,19 +11,17 @@ X = data
 y = predict
 
 
-X_train,X_test,y_train,y_test = train_test_split(X,y)
+X_train,X_test,y_train,y_test = train_test_split(X,y, train_size = 0.7, test_size = 0.3)
 
 model = DecisionTreeClassifier()
 model.fit(X_train,y_train)
 
 p_train = model.predict(X_train)
 p_test = model.predict(X_test)
-prob = model.predict_proba(X_train)
-probability = model.predict_proba(X_test)
 
 acc_train = accuracy_score(y_train, p_train)
 acc_test = accuracy_score(y_test, p_test)
 
-#print( f'Train {acc_train}, test {acc_test}')
-print( f'Train {prob}, test {probability}')
+print(confusion_matrix(y_test, p_test))
 
+print( f'Train {acc_train}, test {acc_test}')
--- ConcatenaTutto.py
+++ ConcatenaTutto.py
@@ -11,6 +11,8 @@ for filename in all_files:
     df = pd.read_csv(filename, index_col=None, header=0)
     li.append(df)
 
+print(li)
+
 result = pd.concat(li, axis=0, ignore_index=True, sort = False) #per combinare più files uno sotto l' altro
 
 result.to_csv('dati/datiCompleti/dataset.csv', index=False, encoding='utf-8-sig')
